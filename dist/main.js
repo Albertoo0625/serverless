4c22584a3a37e81d15fc4d821ffe3bcbb3620d52
Albert Too Kipchirchir <tooalbert79@gmail.com>
2023-05-26 15:05:40 +0300

    working dynamic url

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-async function geturl() {
+async function playStream(){
   try {
     const functiongeturlLocation = "/.netlify/functions/handlerequest";
     const response = await fetch(functiongeturlLocation, {
@@ -7,44 +7,8 @@ async function geturl() {
         'Content-Type': 'application/json'
       }
     });
-
-    const data = await response.json(); // Parse the response JSON
-    const url = data.url; // Extract the URL from the response
-
-    console.log('Received URL:', url);
-
-    // Call the serverless function with the received URL
-    const result=await callServerlessFunction(url);
-
-    console.log(result);
-
-    console.log('callserverlessfunction Called');
-  } catch (error) {
-    console.error('Error in geturl:', error);
-  }
-}
-
-geturl();
-
-
-
-
-async function callServerlessFunction(url) {
-  try {
-    const functionLocation="/.netlify/functions/stream";
-    const response = await fetch(functionLocation, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ url })
-    });
-    const data=console.log(response.data)
-    
-    console.log('Serverless function response:', data);
-    // Process the response data as needed
-  } catch (error) {
-    console.error('Error calling serverless function:', error);
+  }catch(err){
+   console.log(err);
   }
 }
 
--- serverless/handlerequest.js
+++ serverless/handlerequest.js
@@ -1,21 +1,110 @@
+const axios = require('axios');
+const { pipeline } = require('stream');
+const ngrok = require('ngrok');
+const http = require('http');
+
+const findAvailablePort = async (port) => {
+  const server = http.createServer();
+  return new Promise((resolve, reject) => {
+    server.on('error', (error) => {
+      if (error.code === 'EADDRINUSE') {
+        resolve(findAvailablePort(port + 1));
+      } else {
+        reject(error);
+      }
+    });
+    server.listen(port, () => {
+      server.close(() => {
+        resolve(port);
+      });
+    });
+  });
+};
+
 exports.handler = async (event, context) => {
-    try {
-      const requestBody = JSON.parse(event.body);
-      const url = requestBody.url;
-  
-      console.log(`REQUEST URL: ${url}`);
-  
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ url: url, message: 'URL received successfully' }),
-      };
-    } catch (error) {
-      console.error('Error handling request:', error);
-  
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: 'Internal Server Error' }),
-      };
-    }
-  };
-  
+  let ngrokUrl; // Declare ngrokUrl outside the try block
+
+  try {
+    const requestBody = JSON.parse(event.body);
+    const url = requestBody.url;
+
+    console.log(`REQUEST URL: ${url}`);
+
+    const handleStream = async (req, res) => {
+      try {
+        const response = await axios.get(url, {
+          responseType: 'stream',
+        });
+
+        // Set the appropriate headers for the response
+        res.setHeader('Content-Type', 'audio/mpeg');
+        res.setHeader('Transfer-Encoding', 'chunked');
+
+        pipeline(response.data, res, (error) => {
+          if (error) {
+            console.error('Pipeline encountered an error:', error);
+          }
+        });
+      } catch (error) {
+        console.error('Error handling stream:', error);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      }
+    };
+
+    // Create an HTTP server
+    const server = http.createServer(handleStream);
+
+    // Find the next available port
+    const port = await findAvailablePort(3000);
+
+    // Start the server and expose it with ngrok
+    await new Promise((resolve, reject) => {
+      ngrok.kill();
+      server.listen(port, async () => {
+        console.log(`Server is running on port ${port}`);
+
+        try {
+          ngrokUrl = await ngrok.connect({
+            authtoken: '2KVGmlxJUHWrgTXlIU9wtesvpM3_39DmFsdbs5eBcQsustWvy',
+            addr: port, // Use the available port
+            region: 'in', // Replace with your desired ngrok region
+          });
+
+          console.log('ngrok connected:', ngrokUrl);
+
+          // Close ngrok connection when Node.js exits
+          process.on('exit', () => {
+            ngrok.kill();
+          });
+
+          resolve({
+            statusCode: 200,
+            body: JSON.stringify({
+              ngrokUrl: ngrokUrl,
+            }),
+          });
+        } catch (error) {
+          console.error('Error starting ngrok:', error);
+          reject({
+            statusCode: 500,
+            body: 'Internal Server Error',
+          });
+        }
+      });
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ngrokUrl: ngrokUrl,
+      }),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error',
+    };
+  }
+};
